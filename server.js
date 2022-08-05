2c9ed59059467270ccc3570033066d5b674c38bb
tolgaulukan <tolga.ulukan@outlook.com>
2022-08-05 15:50:23 +1000

    update

--- server.js
+++ server.js
@@ -7,20 +7,6 @@ const app = express();
 app.use(express.static('./client/build'))
 app.use(express.json())
 
-let db;
-if (process.env.NODE_ENV === 'production') {
-  db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-} else {
-  db = new pg.Pool({
-    database: 'my_local_database_name',
-    password: 'optional_password' // If you have a password on your local db
-  })
-}
 
 app.use((req, res, next) => {
   console.log(`I am middleware! Request ${req.path}`)
