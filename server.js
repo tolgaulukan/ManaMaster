ee4192f97af75e4c1a3b545b140af14cacebafb9
tolgaulukan <tolga.ulukan@outlook.com>
2022-08-06 11:28:00 +1000

    update

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.static('./client/build'))
 app.use(express.json())
 
 /* final catch-all route to index.html defined last */
-server.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/client/build/index.html');
+ server.get('/*', (req, res) => {
+   res.sendFile(__dirname + '/client/build/index.html');
 })
 
 app.use((req, res, next) => {
