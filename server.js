5250134b3b35af9eea3eb1fd5d57ab89e017e63f
tolgaulukan <tolga.ulukan@outlook.com>
2022-08-06 11:33:10 +1000

    update

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ app.use(express.static('./client/build'))
 app.use(express.json())
 
 /* final catch-all route to index.html defined last */
- server.get('/*', (req, res) => {
-   res.sendFile(__dirname + '/client/public/index.html');
-})
-
-app.use((req, res, next) => {
-  console.log(`I am middleware! Request ${req.path}`)
-  next()
+ app.get('/*', (req, res) => {
+   res.sendFile(__dirname + '/client/build/index.html');
 })
 
 app.listen(port, () => {
