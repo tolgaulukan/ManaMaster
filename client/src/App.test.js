dc280eefbe523dead6711e918918f5509b016139
tolgaulukan <tolga.ulukan@outlook.com>
2022-08-06 13:05:03 +1000

    FINAL

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ManaClicker from '../src/components/ManaClicker'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<ManaClicker />);
+  const linkElement = screen.getByText(/mana/i);
   expect(linkElement).toBeInTheDocument();
 });
